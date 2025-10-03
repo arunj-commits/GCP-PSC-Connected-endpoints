9d388f196169cf02f39e956b8d026bf9ffee1228
arunj-commits <arunj170891@gmail.com>
2025-10-03 16:15:19 +0530

    Add files via upload

---
+++ modules/psc-endpoint/variables.tf
@@ -0,0 +1,44 @@
+# Variables for the Private Service Connect Endpoint module
+
+variable "project_id" {
+  description = "The ID of the project where the PSC endpoint is created."
+  type        = string
+}
+
+
+variable "region" {
+  description = "The region where the PSC endpoint and IP address are provisioned."
+  type        = string
+}
+
+variable "endpoint_name" {
+  description = "The name for the Private Service Connect endpoint (forwarding rule)."
+  type        = string
+  default     = "my-psc-endpoint"
+}
+
+variable "ip_address_name" {
+  description = "The name for the reserved internal IP address."
+  type        = string
+  default     = "psc-endpoint-ip"
+}
+
+variable "subnetwork_self_link" {
+  description = "Self link of the subnetwork for the PSC endpoint (e.g., projects/PROJECT/regions/REGION/subnetworks/SUBNET)."
+  type        = string
+}
+
+variable "network_self_link" {
+  description = "Self link of the VPC network where the PSC endpoint is created (e.g., projects/PROJECT/global/networks/NETWORK)."
+  type        = string
+}
+
+variable "service_attachment_target" {
+  description = "The service attachment URL to connect to (e.g., projects/PROJ/regions/REG/serviceAttachments/ATTACHMENT)."
+  type        = string
+}
+
+variable "static_ip_address" {
+  description = "IP address of the endpoint"
+  type        = string
+}
---
+++ terraforms122.auto.tfvars
@@ -0,0 +1,22 @@
+# psc-root/terraform.tfvars (Example)
+
+psc_endpoints = {
+  #"app_db_endpoint" = {
+  #  region                    = "us-east4"
+  #  endpoint_name             = "app-db-psc-endpoint2"
+  #  ip_address_name           = "app-db-psc-ip-1"
+  #  vpc_network_name          = "psc2"
+  #  subnetwork_name           = "psc5"
+  #  service_attachment_target = "xxxxxxxxxx"
+  #  static_ip_address         = "" # Specifying a static IP
+  #},
+  #"app_db_endpoint3" = {
+  #  region                    = "us-east4"
+  #  endpoint_name             = "app-db-psc-endpoint3"
+  #  ip_address_name           = "app-db-psc-ip-2"
+  #  vpc_network_name          = "psc2"
+  #  subnetwork_name           = "psc6"
+  #  service_attachment_target = "xxxxxxxx"
+  #  static_ip_address         = "" # Specifying a static IP
+  #},
+}
